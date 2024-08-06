3d163b12ad88e813a443285b149410f099e87d90
Joseph Angel <josephmangel01@gmail.com>
2024-08-05 17:12:19 -0700

    new get post

--- src/main/java/joeyTexts/texts/SmsController.java
+++ src/main/java/joeyTexts/texts/SmsController.java
@@ -3,19 +3,15 @@ package joeyTexts.texts;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class SmsController {
 
     @GetMapping
-    public String helloWeb() {
-        return "Hello Web";
+    public String helloWeb(@RequestParam("From") String from) {
+        return from;
     }
 
     @PostMapping(value = "/sendSMS", produces = "application/xml")
@@ -23,6 +19,7 @@ public class SmsController {
         Body body = new Body.Builder("The Robots are coming! Head for the hills!").build();
         Message sms = new Message.Builder().body(body).build();
         MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
+        System.out.println();
         return twiml.toXml();
     }
 }
