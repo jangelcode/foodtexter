7112e156bc14549f5dfef4c8e232c50ee38a09ae
Joseph Angel <josephmangel01@gmail.com>
2024-08-06 11:03:18 -0700

    add emoji

--- src/main/java/joeyTexts/texts/SMSController.java
+++ src/main/java/joeyTexts/texts/SMSController.java
@@ -14,13 +14,15 @@ public class SMSController {
     public String receiveSms(@RequestBody String request, @RequestParam("From") String from) {
         //check if not in cache, and send sign up if not
         if (!Cache.checkInCache(from)){
-            Messages.signUp(from);
+//            uncomment for production
+//            Messages.signUp(from);
+            Messages.sendTextMessage(from, "Got your number.");
             System.out.println("Number added to cache");
             dbUtil.addNumberToDatabase(from);
             System.out.println("Number added to database");
         }
         else {
-            Messages.sendTextMessage(from, "\uD83D\uDC4D");
+            Messages.sendTextMessage(from, ":o");
         }
         return "twiml.toXml()";
     }
