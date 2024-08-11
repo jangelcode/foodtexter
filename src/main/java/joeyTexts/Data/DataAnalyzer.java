2b9d4a88539b7adb483cf7b49212a4108465ede9
Joseph Angel <josephmangel01@gmail.com>
2024-08-10 21:20:47 -0700

    fix data analyzer

--- src/main/java/joeyTexts/Data/DataAnalyzer.java
+++ src/main/java/joeyTexts/Data/DataAnalyzer.java
@@ -14,24 +14,29 @@ public class DataAnalyzer {
 
     public String getWeeklyFoods(String phoneNumber) {
         String weeklyFoodsQuery = "Select * FROM foods WHERE recorded_at BETWEEN (NOW() AT TIME ZONE 'MST' - INTERVAL '7 days') AND NOW() AT TIME ZONE 'MST'";
+        StringBuilder foodsResult = new StringBuilder();
 
         try (Connection connection = getConnection();
              PreparedStatement selectStatement = connection.prepareStatement(weeklyFoodsQuery)) {
 
             // Set the phone number parameter in the SELECT statement
             selectStatement.setString(1, phoneNumber);
+
             try (ResultSet resultSet = selectStatement.executeQuery()) {
-                // Check if the phone number exists in the database
-                if (!resultSet.next()) {
-                    // Set the phone number parameter in the INSERT statement and execute the update
-                    selectStatement.setString(1, phoneNumber);
-                    selectStatement.executeUpdate();
+                // Process the results
+                while (resultSet.next()) {
+                    if (foodsResult.length() > 0) {
+                        foodsResult.append("\n");
+                    }
+                    foodsResult.append(resultSet.getString("food_name"));
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            return "Error retrieving foods: " + e.getMessage();
         }
+        return foodsResult.toString();
     }
-
 }
+
 
